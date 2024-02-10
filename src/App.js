424cd797b8f529e666024793c25a75d03e393132
daghassi <79544966+daghassi@users.noreply.github.com>
2024-02-10 02:57:53 -0700

    Completed framework for journal view landing page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+
+function NavBar() {
+  return (
+    <nav>
+      <div className="nav-name">Journal Log</div>
+      </nav>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <NavBar />
+      <div className="firstBox">
+      <img src="/images/R.png" alt="img" className="firstImg" />
+        <h1 className="title">Title</h1>
+        <h1 className="date-time">date/time</h1>        
+        <p className="body">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      </div>
+      <div className="box">
+      <img src="/images/R.png" alt="img" className="img" />
+        <h1 className="title">Title</h1>
+        <h1 className="date-time">date/time</h1>
+        <p className="body">
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+          nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+          deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <div className="box">
+      <img src="/images/R.png" alt="img" className="img" />
+        <h1 className="title">Title</h1>
+        <h1 className="date-time">date/time</h1>
+        <p className="body">
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+          nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+          deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <div className="box">
+      <img src="/images/R.png" alt="img" className="img" />
+        <h1 className="title">Title</h1>
+        <h1 className="date-time">date/time</h1>
+        <p className="body">
+          nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+          deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <div className="box">
+      <img src="/images/R.png" alt="img" className="img" />
+        <h1 className="title">Title</h1>
+        <h1 className="date-time">date/time</h1>
+        <p className="body">
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+          nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+          deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <header className="App-header">
       </header>
     </div>
   );
