3baae9ce5b2c59b632f7548b3c85319fdca9219b
Tavin Turner <camelinatardis@gmail.com>
2024-02-11 03:49:31 -0700

    User association

--- mailers/response_handler.go
+++ mailers/response_handler.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 
+	"firebase.google.com/go/v4/auth"
 	cloudmailin "github.com/cloudmailin/cloudmailin-go"
+	"google.golang.org/api/iterator"
 )
 
 func serveIncomingMail(firestate *FirebaseState) {
@@ -18,16 +21,45 @@ func serveIncomingMail(firestate *FirebaseState) {
 	})
 
 	http.HandleFunc("/request_notification", func(w http.ResponseWriter, req *http.Request) {
-		decoder := json.NewDecoder(req.Body)
-		var r WebResponse
-		err := decoder.Decode(&r)
+		err := allDocs(context.Background(), firestate)
 		if err != nil {
-			panic(err)
+			log.Fatalln(err)
 		}
-		fmt.Println(r)
+		// decoder := json.NewDecoder(req.Body)
+		// var r WebResponse
+		// err := decoder.Decode(&r)
+		// if err != nil {
+		// 	panic(err)
+		// }
+		// fmt.Println(r)
 	})
 
 	http.ListenAndServe(":3000", nil)
+}
+
+func getEmailFromUID(ctx context.Context, uid string, firestate *FirebaseState) *auth.UserRecord {
+	u, err := firestate.Auth.GetUser(ctx, uid)
+	if err != nil {
+		log.Fatalf("error getting user %s: %v\n", uid, err)
+	}
+	log.Printf("Successfully fetched user data: %v\n", u)
+	return u
+}
+
+func allDocs(ctx context.Context, firestate *FirebaseState) error {
+	fmt.Println("All cities:")
+	iter := firestate.Store.Collection("Reminders").Documents(ctx)
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		fmt.Println(doc.Data()["userID"])
+	}
+	return nil
 }
 
 func sendInFuture(addr string, name string, content string, when int) {
